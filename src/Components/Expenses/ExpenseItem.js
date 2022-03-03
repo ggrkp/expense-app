30163fe32d1436af746ba79d42a4c018d0d2776c
Georgakopoulos Giorgos <g.georgak.77@gmail.com>
2022-03-03 11:56:24 +0200

    some comments

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -8,13 +8,13 @@ const ExpenseItem = (props) => {
   // Initial state mesw tou useState Hook. Sto prwto argument pairnei thn current
   // timi tis metavlitis (expTitle = props.title) kai sto deutero argument einai mia function
   // pou mporei na allazei thn metavliti auth (expTitle = 'Updated')
-  const [expTitle, setTitle] = useState(props.title);
+  const [expTitle, setTitle] = useState(props.title); // Deconstruction enos pinaka.
  
   let expDate = props.date;
   let expAmount = props.amount;
 
   const clickHandler = () => {
-    setTitle('Updated')
+    setTitle('Updated') // Schedules the change soon!
   }
 
   // some helper variables
