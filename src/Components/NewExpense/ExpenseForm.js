b865c367bdb343cbcf0b4be270293587221174f5
ggrkp <ggeorg.prime@gmail.com>
2022-03-03 16:38:42 +0200

    Update state alternatives

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,17 +2,46 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [inputTitle, setInputTitle] = useState(""); // Arxikopoihsh tou title me empty string
+  // ! Dinatothta na exw 1 state gia kathe difatoretiko input. To pio sunithes na sumbei.
+  const [inputTitle, setInputTitle] = useState(""); //  Arxikopoihsh tou title me empty string
   const [inputAmount, setInputAmount] = useState("");
-  const [inputDate, setInputDate] = useState(""); 
+  const [inputDate, setInputDate] = useState("");
+
+  // ! Dinatothta na exw ena state object anti gia 3 diaforetika states( 1 gia kathe input )
+  //   const [userInput, setUserInput] = useState({
+  //     inputTitle: "",
+  //     inputAmount: "",
+  //     inputDate: "",
+  //   });
 
   const titleChangeHandler = (e) => {
     setInputTitle(e.target.value);
+    // ! Lathos tropos na kanw update state otan eksartomai apo palio version ths state (prevstate)
+    // ! Νot Safe Way. Mporei na kataliksw se lathos state snapshot. 
+    // setUserInput({
+    //   ...userInput,
+    //   inputTitle: e.target.value,
+    // });
+    // ! Swstos troposo na kanw update state otan eksartomai apo palio version ths state
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     inputTitle: e.target.value,
+    //   };
+    // });
   };
   const amountChangeHandler = (e) => {
+    // setUserInput({
+    //     ...userInput,
+    //     inputAmount: e.target.value,
+    //   });
     setInputAmount(e.target.value);
   };
   const dateChangeHandler = (e) => {
+    // setUserInput({
+    //     ...userInput,
+    //     inputDate: e.target.value,
+    //   });
     setInputDate(e.target.value);
   };
 
@@ -21,7 +50,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title: </label>
-          <input type="text" onChange={titleChangeHandler} value={inputTitle} />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount:</label>
@@ -30,7 +59,6 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
-            value={inputAmount}
           />
         </div>
         <div className="new-expense__control">
@@ -40,7 +68,6 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
-            value={inputDate}
           />
         </div>
       </div>
